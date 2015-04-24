62d25061cf73b8c2bfd9a586a569e9763c7bf810
Fabian Träger <traeger@praemandatum.de>
2015-04-24 15:14:50 +0200

    New annotation type "RequiredInJson"

---
+++ src/main/java/de/qabel/core/config/RequiredInJson.java
@@ -0,0 +1,6 @@
+package de.qabel.core.config;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+public @Retention(RetentionPolicy.RUNTIME) @interface RequiredInJson { }
